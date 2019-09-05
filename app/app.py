855198bc7069ce871ce39a1b84185f3b8ea025b9
luis acerv <l.arr.cerv@gmail.com>
2019-09-05 00:10:34 -0500

    update routes

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import falcon
 from falcon_auth import FalconAuthMiddleware, TokenAuthBackend
 from app.routes.create_user import CreateUser
-from app.routes.create_client import CreateClient
+from app.routes.create_project import CreateProject
 
 # a loader function to fetch user from username, password
 user_loader = lambda username, password: { 'username': username }
@@ -9,9 +9,9 @@ user_loader = lambda username, password: { 'username': username }
 # basic auth backend
 token_auth = TokenAuthBackend(user_loader=lambda token: {'id':'123'})
 
-auth_middleware = FalconAuthMiddleware(token_auth, exempt_routes=['/api/v1/create-client'])
+auth_middleware = FalconAuthMiddleware(token_auth, exempt_routes=['/api/v1/create-project'])
 
 api = falcon.API(middleware=[auth_middleware])
 
 api.add_route('/api/v1/create-user', CreateUser())
-api.add_route('/api/v1/create-client', CreateClient())
+api.add_route('/api/v1/create-project', CreateProject())
