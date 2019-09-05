2c395b6441608dc9ee39662d28c78829289da60d
luis acerv <l.arr.cerv@gmail.com>
2019-09-04 21:32:35 -0500

    add middleware

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
 import falcon
+from falcon_auth import FalconAuthMiddleware, TokenAuthBackend
 from app.routes.create_user import CreateUser
 
-api = falcon.API()
+# a loader function to fetch user from username, password
+user_loader = lambda username, password: { 'username': username }
+
+# basic auth backend
+token_auth = TokenAuthBackend(user_loader=lambda token: {'id':'123'})
+
+auth_middleware = FalconAuthMiddleware(token_auth, exempt_routes=['/api/v1/create-user'])
+
+api = falcon.API(middleware=[auth_middleware])
 
 api.add_route('/api/v1/create-user', CreateUser())
